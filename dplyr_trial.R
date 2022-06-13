f149180570de425459472f3040c2693fd1daa69c
kkitonga <104992744+kkitonga@users.noreply.github.com>
2022-06-13 09:29:41 -0700

    Add files via upload
    This scripts covers the first 8 pages of the DPLYR package module. We code explores the code filter and arrange to some extent in the following ways:
    wrangling
    exploration
    visualization

---
+++ dplyr_trial.R
@@ -0,0 +1,105 @@
+
+#===================AFFIRMATIONS AND PLAYSLIST==========----
+
+#UNATHAMINIKA! 
+#YOU ARE PRECIOUS!
+#Playlist:ROKIA TRAORE
+#https://www.youtube.com/watch?v=GdfXY3h1CHs
+
+#====================INTRODUCTION======================----
+
+#Script details----
+
+#Prepared by: kkitonga
+#script content :Dplyr
+#link to DPLYR handbook:https://cloud.r-project.org/web/packages/dplyr/dplyr.pdf
+#Date prepared:13/06/2022
+
+#=====================LOAD LIBRARIES====================----
+#load library
+library(dplyr)
+library(DataExplorer)
+library(ggplot2)
+
+#==================1.DATA EXPLORATION==================----
+
+#Data
+data(storms)
+#About
+?storms
+
+#Base R
+View(storms)
+head(storms)
+tail(storms)
+str(storms)
+summary(storms)
+
+#DataExplorer package
+create_report(storms)                        #full report
+
+
+#==========2.FILTER:SUBSETTING AND SORTING DATA=========----
+
+             #Filter function:page 6
+             #Arrange function:page 8
+
+#1.Filter rows based on string variable criteria
+storms %>% 
+ filter(name=="Amy")
+
+#2.Filter rows based on a categorical variable criteria
+storms %>%
+ filter(category==-1)
+
+#3.Filter rows based on more than one condition
+
+storms%>%
+ filter(status=="tropical storm" &  year<1990)
+
+
+#4.Filter then arrange:data wrangling----
+
+storms %>%
+ filter(name %in% c("Amy","Chris","Barry"))%>%
+ arrange(desc(pressure))
+
+
+#=====3:FILTER:SUBSETTING AND SUMMARY STATISTICS===========#
+
+#filter: status==tropical storm and year less than 1990
+#summary statistics:mean and sd for variable pressure
+
+storms %>%
+ filter(status=="tropical storm" &  year<1990)%>%
+ summarize(across(c(pressure),list(mean=mean,sd=sd)))
+
+#filter:only rows where year is less than 1995
+#group by:variable category
+#summary statistics based on variable pressure
+
+storms%>%
+ filter(year<1995)%>%
+ group_by(category)%>%
+ summarize(across(pressure,list(mean=mean,sd=sd)))
+
+#===========4.FILTER:DATA VISUALIZATION================----
+
+#convert data to tibble
+#create time variable and join it to dataset
+#subset rows where DAX>2000
+#plot graph of DAX AND SMI against time
+
+#data 
+data(EuStockMarkets)
+
+EuStockMarkets%>%
+ as_tibble()%>%
+ mutate(time=1:1860)%>%
+ filter(DAX>2000)%>%
+ ggplot(aes(time))+
+ geom_line(aes(y = DAX), color = "blue") +
+ geom_line(aes(y = SMI), color = "green") +  
+ labs(x="time in years",title="Time series plot")
+
+#========================END===============================#
